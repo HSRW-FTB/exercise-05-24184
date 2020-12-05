c3c98d0bcaca5e382e489bbadf7efd00e6efd34f
24184 <74046544+24184@users.noreply.github.com>
2020-12-05 23:17:44 +0100

    Update Clasroom.java

--- src/main/java/Clasroom.java
+++ src/main/java/Clasroom.java
@@ -1,7 +1,8 @@
-
-
+import java.util.Date;
+import java.util.*;
 
 public class Classroom {
+    
     private String id;
     private Course course;
     private Student[] participants;
@@ -9,44 +10,75 @@ public class Classroom {
     private String term;
 
     private Classroom(){}
-
-    public Classroom (Course course, String room){
-
-
-    }
-
-    public Classroom (Course course, String room, java.util.Date Date){
-
-
-    }
-
-    public Course getCourse(){
-        return course;
-    }
-
-    public String getRoom(){
-        return room;
-    }
-
-    public void setRoom(String room){
+    
+    public Classroom(Course course, String room){
+        
+        this.course = course;
         this.room = room;
     }
     
-    public String getTerm(){
-        return term;
+    
+    public Classroom(Course course, String room, Date date){
+        this.course = course;
+        this.room = room;
+    }
+    
+
+    public Course getCourse(){
+        
+        return course;
+    }
+    
+    public String getRoom(){
+        return room;
+    }
+    public void setRoom(String theRoom){
+        this.room = room;
     }
 
-    public void addStudent(Student student ){
-
-        participants = new Student[100];
-        int i;
-
-        for (i=0; i<100; i++ ){
-
-        participants[i] = student;
+    public String getTerm(){
         
+        Calendar calendar = new GregorianCalendar();
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        if(month > 2 && month < 10)
+        {
+            term = "SS" + year;
+            return term;
         }
+        else
+        {
+            term = "WS" + year + "/" + ((year % 2000) + 1);
+            return term;
+        }
+    }
 
+    public String getID(){
+        return id;
+    }
 
+    public void addStudent(Student student){
+        
+            for (int i = 0; i < 100; i++){
+                participants = new Student[100];
+                participants[i] = student;
+            }
+    
+        }
+    public String getParticipantsEmail(){
+        int i = 0;
+        if (i < participants.length){
+            i++;
+            //String studentid = Arrays.toString(participants);
+            
+        }
+        return "";
+        
+    }
+
+    public int getTotalPaticipants(){
+        
+        return participants.length;
     }
 }
