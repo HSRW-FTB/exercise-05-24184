eb607724eb3c267dafc4d10626d13656e5c99fa1
24184 <74046544+24184@users.noreply.github.com>
2020-12-05 23:13:38 +0100

    Update BucketOfObjects.java

--- src/main/java/BucketOfObjects.java
+++ src/main/java/BucketOfObjects.java
@@ -1,1 +1,40 @@
 
+public class BucketOfObjects {
+   
+    private GeometricObject[] items;
+
+    public BucketOfObjects() {}
+
+    public void addObject(GeometricObject obj)
+    {
+        for(int i = 0; i < items.length; i++)
+        {
+            items[i] = obj;
+        }
+    }
+
+    public double getTotalArea()
+    {
+        double Area = 0;
+        for(int i = 0; i < items.length; i++)
+       {
+            return Area = Area + items[i].getArea();
+       }
+       return 0;
+    }
+    
+    public double getTotalPerimeter()
+    {
+        double Perimeter = 0;
+        for(int i = 0; i < items.length; i++)
+       {
+            return Perimeter = Perimeter + items[i].getPerimeter();
+       }
+       return 0;
+    }
+
+    public String toString()
+    {
+        return "???";
+    }
+}
