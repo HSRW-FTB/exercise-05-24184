cc600b4db17cbe0eabbb6dd84c50aa76050a2d82
24184 <74046544+24184@users.noreply.github.com>
2020-12-05 23:01:51 +0100

    Update Rectangle.java

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,1 +1,33 @@
 
+public class Rectangle extends GeometricObject{
+
+    double width  = 1;
+    double height = 1;
+    
+    private Rectangle(){
+    
+    }
+    
+    public Rectangle(double width, double height){
+      this.width =  width;
+      this.height = height;
+    
+    }
+    
+    public double getArea(){
+    return width*height;
+    }
+  
+    public double getPerimeter(){
+    return (width+height)*2;
+    
+    }
+
+    public void changeColor(String color){
+
+        super.setColor(color);
+
+    }
+  
+  
+  }
