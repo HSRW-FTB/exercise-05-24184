7672cba8f1234bcee435ebd578bc863fb43a8308
24184 <74046544+24184@users.noreply.github.com>
2020-12-05 22:46:13 +0100

    Update Circle.java

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,1 +1,39 @@
 
+public class Circle {
+    double radius = 1;
+
+    Circle(){}
+
+    public double getArea(){
+
+        return (Math.PI * (radius * radius));
+
+    }
+
+
+    public double getPerimeter(){
+
+        return (Math.PI * radius * 2);
+
+    }
+
+
+    public void setRadius(double radius){
+
+        this.radius = radius;
+
+    }
+
+
+    public void printCircle(){
+
+        System.out.println("The circle is created with the radius: " + radius);
+
+    }
+
+
+    public String toString(){
+
+        return "Circle: radius = " +radius;
+    }
+}
